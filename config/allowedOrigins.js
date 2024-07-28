2492d82276a8ee575cc50861f82151ba981b1721
Vokechordz <Olomuvokee@gmail.com>
2024-07-28 19:34:19 +0100

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://productfeedback-ykyt.onrender.com'
+    'https://productfeedback-hwj5.onrender.com'
 ]
 
 module.exports = allowedOrigins
