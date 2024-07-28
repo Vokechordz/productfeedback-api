379391dcaa44d49fb1ff769239272f8a13272505
Vokechordz <Olomuvokee@gmail.com>
2024-07-28 21:34:12 +0100

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://productfeedback-hwj5.onrender.com'
+    'https://productfeedback-mu.vercel.app/'
 ]
 
 module.exports = allowedOrigins
